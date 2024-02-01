91ff5b75c2a8563b7953e32928e7627a96315552
Nikstore <nikstore@mail.ru>
2024-02-01 21:51:56 +0300

    my new commit!!2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,7 @@ public class Main {
         System.out.println();
         System.out.println("всё отлично!");
         System.out.println("всё круто");
+        System.out.println();
+        System.out.println();
     }
 }
